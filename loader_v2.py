c2d5d127c9c5c33e3ca0dcf402058a2a57eda632
Furqan Abid <furqanabid@kaist.ac.kr>
2022-01-14 17:50:08 +0900

    testing

--- loader_v2.py
+++ loader_v2.py
@@ -5,24 +5,36 @@ import torch
 import os
 import numpy as np
 import timeit
+import tqdm
 from torch.utils.data import DataLoader
 
 
 
 root='/home/share/dataset/semanticKITTI'
 pc_root='E:/Datasets/SemanticKitti/dataset/Kitti'
-# laptop_root ='/media/furqan/Terabyte/Lab/datasets/semanticKitti'
+laptop_root ='/media/furqan/Terabyte/Lab/datasets/semanticKitti'
 DATA = yaml.safe_load(open('params/semantic-kitti.yaml', 'r'))
 ARCH = yaml.safe_load(open('params/arch-params.yaml', 'r'))
 
 
 train = 'train'
-train_dataset=SemanticKitti(root=pc_root,sequences=DATA["split"]["train"],labels=DATA["labels"],
+multi_dataset=SemanticKitti(root=laptop_root,sequences=['3'],labels=DATA["labels"],
                             color_map=DATA["color_map"],learning_map=DATA["learning_map"],learning_map_inv=DATA["learning_map_inv"],
                             sensor=ARCH["dataset"]["sensor"],multi_proj=ARCH["multi"],max_points=ARCH["dataset"]["max_points"],train=train)
 
-dataloader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=0,
+multi_dataloader = DataLoader(multi_dataset, batch_size=2, shuffle=False, num_workers=12,
                         pin_memory=True, drop_last=True)
+
+
+original_dataset=SemanticKitti(root=laptop_root,sequences=['3'],labels=DATA["labels"],
+                            color_map=DATA["color_map"],learning_map=DATA["learning_map"],learning_map_inv=DATA["learning_map_inv"],
+                            sensor=ARCH["dataset"]["sensor"],multi_proj=ARCH["single"],max_points=ARCH["dataset"]["max_points"],train=train)
+
+original_dataloader = DataLoader(original_dataset, batch_size=2, shuffle=False, num_workers=12,
+                        pin_memory=True, drop_last=True)
+
+
+
 
 time_taken = timeit.default_timer()
 
@@ -33,27 +45,25 @@ data_size = 0
 feature_means = np.array([0, 0, 0, 0, 0])
 feature_std = np.array([0, 0, 0, 0, 0])
 
+multi_time_taken=[]
+original_time_taken=[]
+
 for batch_idx, (proj_multi_temporal_scan,proj_multi_temporal_label,scan_points,scan_range,
-                scan_remission,scan_labels,proj_single_label,pixel_u,pixel_v) in enumerate (dataloader):
-    # print ("batch ",batch_idx,"time taken",timeit.default_timer() - time_taken)
-
-    proj_multi_temporal_scan=proj_multi_temporal_scan.numpy()
-
-    bs,ts,mr,ft,h,w=np.shape(proj_multi_temporal_scan)
-
-    # calculate mean and s.d for each range
-    for features in range(ft):
-        array=proj_multi_temporal_scan[0,0,0,features,:,:]
-        feature_means[features]+=np.mean(array)
-        feature_std[features]+=np.std(array)
-        print("mean :", feature_means[features], "std :", feature_std[features], "batch_idx : ", batch_idx ,"feature",features)
+                scan_remission,scan_labels,proj_single_label,pixel_u,pixel_v) in tqdm.tqdm(enumerate (multi_dataloader)):
+    multi_time_taken.append(timeit.default_timer() - time_taken)
+    time_taken = timeit.default_timer()
 
 
-    data_size+=1
-    # input=proj_multi_temporal_scan.cuda()
-    # labels=proj_multi_temporal_label.cuda(non_blocking=True).long()
-    # time_taken =timeit.default_timer()
+time_taken = timeit.default_timer()
 
+for batch_idx, (proj_multi_temporal_scan,proj_multi_temporal_label,scan_points,scan_range,
+                scan_remission,scan_labels,proj_single_label,pixel_u,pixel_v) in tqdm.tqdm(enumerate (original_dataloader)):
+
+    original_time_taken.append(timeit.default_timer() - time_taken)
+    time_taken = timeit.default_timer()
+
+print(np.array(multi_time_taken).mean())
+print(np.array(original_time_taken).mean())
 
 overall_mean=feature_means/data_size
 overall_std=feature_std/data_size
